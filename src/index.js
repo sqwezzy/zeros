8d896d5f2457c0ac7517315a4e8fc3b640606b87
sqwezzy <maks.subbotko46@gmail.com>
2018-09-30 15:54:28 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    var zeros = 0;
+    while (number >= 5) {
+		zeros += Math.floor(number / 5);
+		number = Math.floor(number / 5);
+    }
+    return zeros;
+};
